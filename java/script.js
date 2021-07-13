a9cc6c720565bd291c54498036f6277fdb10da75
Vicente Mesa Torres <vmestor@hotmail.com>
2021-07-13 14:06:05 +0100

    CV
    CurriculumVitae Vicente Mesa

---
+++ java/script.js
@@ -0,0 +1,28 @@
+//Funciones javascript
+//Referentes a los diferentes botones
+function Dat(){
+	window.open('./part/part1.html', 'visualizar');
+}
+
+function For(){
+	window.open('./part/part2.html', 'visualizar');
+}
+
+function Adi(){
+	window.open('./part/part3.html', 'visualizar');
+}
+
+function Exp(){
+	window.open('./part/part4.html', 'visualizar');
+}
+
+function Dest(){
+	window.open('./part/part5.html', 'visualizar');
+}
+
+function Proy(){
+	window.open('./part/part6.html', 'visualizar');
+}
+
+
+
